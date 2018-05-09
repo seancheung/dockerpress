f4227b19f333b1f2170dcbd958e75b4cfd335137
Sean Cheung <theoxuanx@gmail.com>
2018-05-10 00:08:55 +0800

    First commit

---
+++ entrypoint.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+set -e
+
+MYSQL_INITSQL=/var/run/mysqld/.init
+
+function boot_mysql()
+{
+    bootfile=$1
+    echo "[Mysql] secure installation"
+    echo "USE mysql;" > $bootfile;
+    echo "UPDATE user SET password=PASSWORD('') WHERE User='root' AND host='localhost';" >> $bootfile
+    echo "DELETE FROM user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');" >> $bootfile
+    echo "DELETE FROM user WHERE User='';" >> $bootfile
+    echo "DELETE FROM db WHERE Db LIKE 'test%';" >> $bootfile
+    echo "DROP DATABASE test;" >> $bootfile
+
+    if [ -n "$MYSQL_ROOT_PASSWORD" ]; then
+        echo "[Mysql] updating root password"
+        echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' WITH GRANT OPTION;" >> $bootfile
+    fi
+
+    # MYSQL_USER: "username:password" or "username". password will be the same as username if omitted.
+    # For multiple user creation, seperate them with ";".
+    if [ -n "$MYSQL_USER" ]; then
+        IFS=';'; users=($MYSQL_USER); unset IFS;
+        for entry in "${users[@]}"; do
+            IFS=':'; sub=($entry); unset IFS;
+            if [ ${#sub[@]} -eq 1 ]; then
+                username=${sub[0]}
+                password=$username
+            elif [ ${#sub[@]} -eq 2 ]; then
+                username=${sub[0]}
+                password=${sub[1]}
+            else
+                echo "[Mysql] invalid username in ${MYSQL_USER}"
+                exit 1
+            fi
+            echo "[Mysql] create user ${username}"
+            echo "CREATE USER IF NOT EXISTS '${username}'@'%' IDENTIFIED BY '${password}';" >> $bootfile
+        done
+    fi
+
+    # MYSQL_DATABASE: "username@database" or "database". username will be the same as database if omitted.
+    # User will be created(with password the same as username) if not exist.
+    # For multiple database creation, seperate them with ";".
+    if [ -n "$MYSQL_DATABASE" ]; then
+        IFS=';'; ary=($MYSQL_DATABASE); unset IFS;
+        for entry in "${ary[@]}"; do
+            IFS='@'; sub=($entry); unset IFS;
+            if [ ${#sub[@]} -eq 1 ]; then
+                username=${sub[0]}
+                database=$username
+            elif [ ${#sub[@]} -eq 2 ]; then
+                username=${sub[0]}
+                database=${sub[1]}
+            else
+                echo "[Mysql] invalid database in ${MYSQL_DATABASE}"
+                exit 1
+            fi
+            echo "[Mysql] create database ${database}"
+            echo "CREATE DATABASE IF NOT EXISTS \`${database}\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $bootfile
+            # ensure user exists
+            echo "CREATE USER IF NOT EXISTS '${username}'@'%' IDENTIFIED BY '${username}';" >> $bootfile
+            echo "[Mysql] grant privileges to ${username} on ${database}"
+            echo "GRANT ALL PRIVILEGES ON \`${database}\`.* to '${username}'@'%';" >> $bootfile
+        done
+    fi
+
+    echo "FLUSH PRIVILEGES;" >> $bootfile
+
+    echo "[Mysql] initializing database"
+    mysql_install_db --user=mysql --datadir=/var/opt/mysql
+}
+
+
+if [ ! -f "$MYSQL_INITSQL" ]; then
+    if [ -z "$MYSQL_SKIP_INIT" ]; then
+        boot_mysql "$MYSQL_INITSQL"
+    else
+        touch "$MYSQL_INITSQL"
+    fi
+fi
+
+if [ ! -f '/var/www/localhost/htdocs/index.php' ] && [ -z "$WP_SKIP_DOWNLOAD" ]; then
+    echo "=== downloading latest wordpress tarball ==="
+    curl -sL --progress-bar https://wordpress.org/latest.tar.gz | tar -xvz -C /var/www/localhost/htdocs --strip-components=1
+fi
+
+if [ ! -f '/var/www/localhost/htdocs/.htaccess' ]; then
+cat > '/var/www/localhost/htdocs/.htaccess' << EOF
+# BEGIN WordPress
+<IfModule mod_rewrite.c>
+RewriteEngine On
+RewriteBase /
+RewriteRule ^index\.php$ - [L]
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule . /index.php [L]
+</IfModule>
+# END WordPress
+EOF
+fi
+
+chown -R apache:apache /var/www/localhost/htdocs
+
+exec "$@"
